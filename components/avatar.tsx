1cd3d7d3a8a9281d879c4eeee575fdb79c647d0f
Alex Voievudko <oleksandrv@zeptolab.com>
2023-09-15 19:07:02 +0200

    feat: add header component

---
+++ components/avatar.tsx
@@ -0,0 +1,41 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { useTransition, animated, config } from '@react-spring/web';
+
+import avatar1 from '../public/images/avatar-1.jpg';
+import avatar2 from '../public/images/avatar-2.jpg';
+import avatar3 from '../public/images/avatar-3.jpg';
+import avatar4 from '../public/images/avatar-4.jpg';
+import avatar5 from '../public/images/avatar-5.jpg';
+
+export const Avatar = () => {
+	const images = [avatar1, avatar2, avatar3, avatar4, avatar5];
+	const [index, setIndex] = useState(0);
+
+	const transitions = useTransition(index, {
+		initial: { opacity: 1, transform: 'scale(1)' },
+		from: { opacity: 0, transform: 'scale(0)' },
+		enter: { opacity: 1, transform: 'scale(1)' },
+		leave: { opacity: 0, transform: 'scale(0)' },
+		config: config.stiff,
+		loop: true,
+	});
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIndex((state) => (state + 1) % images.length);
+		}, 10000);
+		return () => clearInterval(interval);
+	}, [images.length]);
+
+	return (
+		<div className='relative w-24 h-24 rounded-full border-4 border-primary flex-shrink-0  overflow-hidden'>
+			{transitions((style, index) => (
+				<animated.div style={style} className='absolute p-2 inset-0 w-full h-full flex items-center justify-center'>
+					<Image src={images[index]} alt='Avatar' priority className='object-cover w-full rounded-full h-full' />
+				</animated.div>
+			))}
+		</div>
+	);
+};
---
+++ components/header.tsx
@@ -0,0 +1,69 @@
+'use client';
+import React from 'react';
+import { animated, useSpring, config } from '@react-spring/web';
+import { Navigation } from '@app/components/nav-list';
+import { Social } from '@app/components/social';
+import { Avatar } from '@app/components/avatar';
+
+export const Header = () => {
+	const avatarSpring = useSpring({
+		from: { opacity: 0, transform: 'translateY(50px)' },
+		to: { opacity: 1, transform: 'translateY(0px)' },
+		config: config.wobbly,
+	});
+
+	const headerSpring = useSpring({
+		from: { opacity: 0, transform: 'translateY(50px)' },
+		to: { opacity: 1, transform: 'translateY(0px)' },
+		config: config.stiff,
+		delay: 200,
+	});
+
+	const subheaderSpring = useSpring({
+		from: { opacity: 0, transform: 'translateY(50px)' },
+		to: { opacity: 1, transform: 'translateY(0px)' },
+		config: config.gentle,
+		delay: 400,
+	});
+
+	const paragraphSpring = useSpring({
+		from: { opacity: 0, transform: 'translateX(-50px)' },
+		to: { opacity: 1, transform: 'translateX(0px)' },
+		config: config.slow,
+		delay: 800,
+	});
+
+	return (
+		<header className='lg:sticky lg:top-0 lg:flex lg:h-screen lg:w-2/5 lg:flex-col flex flex-col gap-8 lg:justify-between lg:py-24'>
+			<div className='flex gap-4 flex-col'>
+				<div className='flex gap-4 items-center'>
+					{/* Avatar */}
+					<animated.div style={avatarSpring}>
+						<Avatar />
+					</animated.div>
+					{/* Details */}
+					<div>
+						<animated.h1 style={headerSpring} className='text-4xl font-bold tracking-tight text-white'>
+							Alex Voievudko
+						</animated.h1>
+						<animated.h2
+							style={subheaderSpring}
+							className='text-2xl bg-clip-text text-transparent bg-primary font-semibold tracking-tight'
+						>
+							Full Stack Developer
+						</animated.h2>
+					</div>
+				</div>
+				<animated.p style={paragraphSpring} className='max-w-md'>
+					Experienced Frontend and Fullstack TypeScript Developer, specializing in React, Node.js, and TypeScript, with
+					over 1 year of commercial expertise and a passion for crafting user-centric applications.
+				</animated.p>
+
+				<Navigation />
+			</div>
+
+			{/* Social */}
+			<Social />
+		</header>
+	);
+};
