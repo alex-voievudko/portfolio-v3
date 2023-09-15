f8f3a753b40babbbfebd1ecb7f2548b53125aeb2
Alex Voievudko <oleksandrv@zeptolab.com>
2023-09-15 19:06:40 +0200

    feat: add about section

---
+++ sections/about.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button } from '@app/components/button';
+import about from '@app/data/about.json';
+
+export const AboutSection = () => {
+	const paragraphs = about.text.split('\n');
+
+	return (
+		<section id='about' aria-label='Aboout Me' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'>
+			<div className='sticky top-0 z-25 -mx-6  w-screen bg-background/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+				<h2 className='text-lg text-secondary font-extrabold uppercase tracking-widest lg:sr-only'>About</h2>
+			</div>
+			<div className='lg:px-4 mt-4'>
+				{paragraphs.map((paragraph, index) => (
+					<p key={`${paragraph.slice(0, 5)}-${index}`} className=' mb-2'>
+						{paragraph}
+					</p>
+				))}
+
+				<Button
+					href='https://drive.google.com/file/d/1v6zQ4gX3ZnYh4q9fZQb3LkYxjO7pZc3H/view?usp=sharing'
+					className='mt-3'
+				>
+					Download Resume
+				</Button>
+			</div>
+		</section>
+	);
+};
