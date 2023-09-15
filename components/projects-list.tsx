ddeb9fea45da716d0407f75e6b197c8ccc3a45a7
Alex Voievudko <oleksandrv@zeptolab.com>
2023-09-16 00:22:00 +0200

    refactor: remove image preview modal

--- components/projects-list.tsx
+++ components/projects-list.tsx
@@ -2,29 +2,24 @@
 import React from 'react';
 import projects from '@app/data/projects.json';
 import { ProjectItem } from '@app/components/project-item';
-import { PreviewModal } from '@app/components/preview-modal';
 
 export const ProjectsList = () => {
 	const [preview, setPreview] = React.useState<string | null>(null);
 
 	return (
-		<>
-			<ul className='group/list flex flex-col gap-12'>
-				{projects.map((project) => (
-					<ProjectItem
-						key={project.title}
-						title={project.title}
-						description={project.description}
-						preview={project.preview}
-						github={project.github}
-						live={project.live}
-						technologies={project.technologies}
-						onImageClick={(image) => setPreview(image)}
-					/>
-				))}
-			</ul>
-
-			<PreviewModal image={preview} visible={preview !== null} onClose={() => setPreview(null)} />
-		</>
+		<ul className='group/list flex flex-col gap-12'>
+			{projects.map((project) => (
+				<ProjectItem
+					key={project.title}
+					title={project.title}
+					description={project.description}
+					preview={project.preview}
+					github={project.github}
+					live={project.live}
+					technologies={project.technologies}
+					onImageClick={(image) => setPreview(image)}
+				/>
+			))}
+		</ul>
 	);
 };
