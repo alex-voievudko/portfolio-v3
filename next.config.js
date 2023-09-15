06ca73310f6d03f6919d7867e6a90a3e437713b8
Alex Voievudko <oleksandrv@zeptolab.com>
2023-09-15 19:04:27 +0200

    build: project set up, tailwind config, install dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		formats: ['image/avif', 'image/webp'],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./sections/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: '#011627',
+				card: '#1c2b3b',
+				primary: '#ff9f1c',
+				secondary: '#2ec4b6', // e71d36
+				typography: '#8b8b8b',
+				white: '#fdfffc',
+			},
+			fontFamily: {
+				roboto: ['"Roboto Mono"', 'monospace'],
+			},
+			backgroundImage: {
+				'gradient-linear': 'linear-gradient(rgb(45, 50, 58), rgb(34, 37, 43) 99%)',
+				'gradient-primary': 'linear-gradient(rgb(250, 84, 0) -7%, rgb(250, 62, 0) 105%)',
+			},
+			boxShadow: {
+				card: 'rgba(99, 104, 135, 0.5) 0px 1px 0px 0px inset',
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
