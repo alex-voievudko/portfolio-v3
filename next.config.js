e978196907eb0a9f52d50d4759df9813a6fa6c50
Alex Voievudko <alex.voievudko@immfly.com>
2024-10-19 11:42:36 +0200

    chore: add biome, update dependencies, move to bun

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		formats: ['image/avif', 'image/webp'],
+		formats: ["image/avif", "image/webp"],
 		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
 	},
+	reactStrictMode: false,
 };
 
 module.exports = nextConfig;
